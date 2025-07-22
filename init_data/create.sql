ffec95ba3fbf97e55ac601cf8c3bf50901ce2f7f
Travis Uhrig <travisformayor@users.noreply.github.com>
2025-07-22 16:26:14 -0600

    seed data for test user

--- init_data/create.sql
+++ init_data/create.sql
@@ -20,3 +20,18 @@ CREATE TABLE flashcards (
   question TEXT NOT NULL,
   answer TEXT NOT NULL
 );
+
+-- Add test user and test deck seed data
+ALTER TABLE "flashcards" ADD FOREIGN KEY ("deck_id") REFERENCES "decks" ("id") ON DELETE CASCADE;
+
+-- Create test user (password: 'password')
+INSERT INTO users (email, password_hash) VALUES ('test@test.com', '$2a$10$jjyRDeguXO.utnkSYahPeufYRyKnL3dCaSux1f3A0uQH8FyBlrR8u'); 
+
+-- The user above gets id=1
+INSERT INTO decks (user_id, title) VALUES (1, 'Capital Cities');
+
+-- The deck above gets id=1
+INSERT INTO flashcards (deck_id, question, answer) VALUES
+(1, 'What is the capital of France?', 'Paris'),
+(1, 'What is the capital of Japan?', 'Tokyo'),
+(1, 'What is the capital of Australia?', 'Canberra');
